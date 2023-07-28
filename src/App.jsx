47805b389b20eef8e67e88f7ded99843f380b994
HernanJr0 <hernanjunior90@gmail.com>
2023-07-27 22:00:18 -0300

    comeco corpo da home

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,18 @@ function App() {
     //data
     var dia = custom_date()
     var loc = "Manaus - AM"
-    
+
     //temperatura
-    var temp = Math.round(Math.random() * 18)+15,
-    t_max = (temp + 3),
-    t_min = (temp - 2),
-    s_temp
-    if(temp>25){
+    var temp = Math.round(Math.random() * 18) + 15,
+        t_max = (temp + 3),
+        t_min = (temp - 2),
+        s_temp
+    if (temp > 25) {
         s_temp = Math.floor(temp * 1.1)
-    }else{
+    } else {
         s_temp = Math.floor(temp * 0.95)
     }
-    
+
 
     //clima
     var climas = ['Ensolarado', 'Parcialmente ensolarado', 'Nublado']
@@ -36,16 +36,16 @@ function App() {
     climaf()
 
     //setup clima
-    function climaf(){
-        if(temp < 22){
+    function climaf() {
+        if (temp < 22) {
             clima = climas[2]
             c_url = nublado
 
-        }else if(temp < 28){
+        } else if (temp < 28) {
             clima = climas[1]
             c_url = parc_ensolarado
 
-        }else if(temp < 34){
+        } else if (temp < 34) {
             clima = climas[0]
             c_url = ensolarado
 
@@ -62,43 +62,43 @@ function App() {
 
     return (
         <div className="App">
-            <Header/>
+            <Header />
             <section id='tudo'>
                 <img id='bg' src={bg} alt="bg" />
                 <div id='data_loc'>
                     <div id='data'>
-                        <a>{dia}</a>
+                        <a href=''>{dia}</a>
                     </div>
                     <div id='loc'>
-                        <a><img src={place} id='looouco'/>{loc}</a>
+                        <a href=''><img src={place} id='looouco' />{loc}</a>
                     </div>
                     <h1 id='clima'>{clima}</h1>
-                    
                 </div>
-                <div id='c_clima'>
 
+                <div id='c_clima'>
                     <div id='clima_report'>
                         <img id='c_img' src={c_url} alt='img_clima' />
                         <h1>{temp}ºC</h1>
                         <div id='max_min'>
-                            <div>{t_max}<img src={arrow_up}/></div>
-                            <div>{t_min}<img src={arrow_down}/></div>
+                            <div>{t_max}<img src={arrow_up} /></div>
+                            <div>{t_min}<img src={arrow_down} /></div>
                         </div>
                     </div>
-
-                    <a>sensação termica: {s_temp} ºC</a>
+                    <a href=''>sensação termica: {s_temp} ºC</a>
                 </div>
                 <div id='stat'>
-                    <div><a>Qualidade do ar: Razoável</a></div>
-                    <div><a>Umidade: 82%</a></div>
-                    <div><a>Vento: 9 km/h</a></div>
+                    <div><a href=''>Qualidade do ar: Razoável</a></div>
+                    <div><a href=''>Umidade: 82%</a></div>
+                    <div><a href=''>Vento: 9 km/h</a></div>
                 </div>
+
             </section>
             <section id='corpo'>
-                <div>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a arcu pellentesque magna elementum porttitor in a arcu. Phasellus eget euismod tortor, in sagittis dui. Quisque ut dolor nisi. Nullam turpis augue, consequat sed risus sit amet, consectetur condimentum orci. Sed sed egestas nulla. Fusce eleifend purus et risus malesuada placerat.
+                <div id='news'>
+                    
                 </div>
             </section>
+
         </div>
     )
 }
