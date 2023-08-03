f4086c134217aeb45e9d560d320e58da89841f82
wMzley <wesleyfelipe1904@gmail.com>
2023-08-03 19:48:49 -0400

    KKKKKKKKKKKKKKKK

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,11 +1,30 @@
-import logoGithub from '../assets/logoGithub.png'
+import logoGithub from '../assets/Github.svg'
+import logoGoogle from '../assets/Google.svg'
+import logoInstagram from '../assets/Instagram.svg'
+import logoLinkedIn from '../assets/LinkedIn.svg'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div id='Footer'>
-            <p>Feito com ❤️ por: Equipe Eco Weather</p>
-            <a href="https://github.com/HernanJr0/ecoweather-website"><img src={logoGithub} id='logoGithub' /></a>
+            <p id='devmsg'>Feito com ❤️ por: Equipe Eco Weather</p>
+            <div id='links'>
+                <a href="https://github.com/HernanJr0/ecoweather-website">
+                    <img src={logoGithub} class='logo' />
+                </a>
+                
+                <a href="">
+                    <img src={logoGoogle} class='logo' />
+                </a>
+
+                <a href="">
+                    <img src={logoInstagram} class='logo' />
+                </a>
+
+                <a href="">
+                    <img src={logoLinkedIn} class='logo' />
+                </a>
+            </div>
         </div>
     )
 }
