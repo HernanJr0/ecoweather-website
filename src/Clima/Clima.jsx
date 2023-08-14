dc339f67fae66b521355e5ca4028d76be894a1d0
wMzley <wesleyfelipe1904@gmail.com>
2023-08-13 20:25:32 -0400

    agora vai p api

--- src/Clima/Clima.jsx
+++ src/Clima/Clima.jsx
@@ -1,67 +1,99 @@
 import React, { Component } from 'react'
+import './Clima.css'
+
+import bg1 from '../assets/background-1.png';
+
+import calendar from '../assets/calendar.png';
+import pin from '../assets/pin.png';
+
 import nublado from '../assets/nublado.png';
 import ensolarado from '../assets/ensolarado.png';
 import parc_ensolarado from '../assets/parc_ensolarado.png';
-import bg1 from '../assets/background-1.png'
+
 import arrow_up from '../assets/up-right-arrow.png';
 import arrow_down from '../assets/down-right-arrow.png';
-import place from '../assets/place.svg';
 
 class Clima extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            dia: this.custom_date()
+            dia: this.custom_date(),
+            loc: '',
+            clima: '',
+            temp: '',
+            max_temp: '',
+            min_temp: '',
+            s_ter: '',
+            humid: '',
+            vento: '',
         }
 
-        this.weatherBalloon = this.weatherBalloon.bind(this)
         this.custom_date = this.custom_date.bind(this)
         this.drawWeather = this.drawWeather.bind(this)
-
+        this.weatherBallon = this.weatherBallon.bind(this)
     }
 
     componentDidMount() {
-        //this.custom_date()
-        this.weatherBalloon(6167865)
+        this.weatherBallon(6167865)
     }
 
-    drawWeather(d) {
-
-        let clima
-        let c_img
-
-        let celcius = Math.round(parseFloat(d.main.temp) - 273.15);
-        let fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
-
-        clima = d.weather.description
-        c_img = d.weather.icon
-
-        document.getElementById('clima').innerHTML += clima;
-        document.getElementById('temp').innerHTML += celcius + '&deg;';
-        document.getElementById('loc').innerHTML += d.name;
-        document.getElementById('c_img').src = c_img;
-    }
-
-    weatherBalloon(cityID) {
+    weatherBallon(CityID) {
         let key = 'd3afafb4de8d7a76ad9ced3bed938d51';
-        
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
-            .then(function (resp) { return resp.json() })
-            .then(function (data) {
-                this.drawWeather(data)
-                
-            }) // Convert data to json
 
+        fetch('https://api.openweathermap.org/data/2.5/weather?id=' + CityID + '&appid=' + key)
+
+            .then((resp) => { return resp.json() })
+            .then((data) => {
+                console.log(data)
+                this.drawWeather(data)
+            }) // Convert data to json
             .catch(function () {
                 // catch any errors
             });
     }
 
+    drawWeather(d) {
+        let c_img
+
+        let t = Math.round(parseFloat(d.main.temp) - 273.15);
+        let max_t = Math.round(parseFloat(d.main.temp_max) - 273.15);
+        let min_t = Math.round(parseFloat(d.main.temp_min) - 273.15);
+        let feel = Math.round(parseFloat(d.main.feels_like) - 273.15);
+
+        //TODO
+        //arruma isso aq manokkkkkk
+        //{
+        if (t < 22) {
+            c_img = nublado
+
+        } else if (t < 28) {
+            c_img = parc_ensolarado
+
+        } else if (t < 34) {
+            c_img = ensolarado
+
+        }
+        //} 
+
+        this.setState({
+            loc: d.name,
+            clima: d.weather[0].description,
+            temp: t,
+            max_temp: max_t,
+            min_temp: min_t,
+            s_ter: feel,
+            humid: d.main.humidity,
+            vento: d.wind.speed
+        });
+
+        document.querySelector('#c_img').src = c_img;
+    }
+
     custom_date() {
         let today = new Date();
-        let custom_months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-        let custom_days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+        let custom_months = ["Jan", "Fev", "Mar", "Abr", "Mao", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+        let custom_days = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sexta", "Sáb"];
         let date = custom_days[today.getDay()] + ", " + today.getDate() + " de " + custom_months[today.getMonth()];
         return date;
     }
@@ -71,30 +103,39 @@ class Clima extends Component {
             <div id='head'>
                 <img id='bg' src={bg1} alt="bg" />
                 <div id='data_loc'>
-                    <div id='data'>
-                        <a href=''>{this.state.dia}</a>
+                    <div id='pegakkk'>
+                        <div id='data'>
+                            <a href=''>
+                                {this.state.dia}<img id='calendar' src={calendar} />
+                            </a>
+                        </div>
+
+                        <div id='loc'>
+                            <a href=''>
+                                <img id='pin' src={pin} />{this.state.loc}
+                            </a>
+                        </div>
                     </div>
-                    <div id='loc'>
-                        <a href=''><img src={place} id='pin' />{/* {loc} */}</a>
-                    </div>
-                    <h1 id='clima'>{/* {clima} */}</h1>
+                    <h1 id='clima'>{this.state.clima}</h1>
                 </div>
 
                 <div id='c_clima'>
                     <div id='clima_report'>
-                        <img id='c_img' /*src= {c_url}*/ alt='img_clima' />
-                        <h1>{/* {temp} */}ºC</h1>
+                        <img id='c_img' alt='img_clima' />
+                        <h1 id='temp'>{this.state.temp}ºC</h1>
                         <div id='max_min'>
-                            <div>{/* {t_max} */}<img src={arrow_up} /></div>
-                            <div>{/* {t_min} */}<img src={arrow_down} /></div>
+                            <div>{this.state.max_temp}<img src={arrow_up} /></div>
+                            <div>{this.state.min_temp}<img src={arrow_down} /></div>
                         </div>
                     </div>
-                    <a href=''>sensação termica: {/* {s_temp} */} ºC</a>
+                    <div id='feel'>Sensação termica: {this.state.s_ter}</div>
                 </div>
                 <div id='stat'>
-                    <div><a href=''>Qualidade do ar: Razoável</a></div>
-                    <div><a href=''>Umidade: 82%</a></div>
-                    <div><a href=''>Vento: 9 km/h</a></div>
+                    {/* porreessaakkkkkkkk */}
+                    <div>Qualidade do ar: Razoável</div>
+
+                    <div id='humid'>Umidade: {this.state.humid} %</div>
+                    <div id='vento'>Vento: {this.state.vento} km/h</div>
                 </div>
             </div>
         )
