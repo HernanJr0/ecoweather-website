072d6e6a19b9922301d66024869258699df8d90b
HernanJr0 <hernanjunior90@gmail.com>
2023-08-15 19:30:16 -0400

    Router ok, inicio tela dicas, mudanças banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,15 @@
-import Header from './Components/Header/Header.jsx';
-import Noticia from './Components/Noticia/Noticia.jsx';
-import Footer from './Components/Footer/Footer.jsx';
-import Atalho from './Components/Atalho/Atalho.jsx'
-import Previsao from './Components/Previsao/Previsao.jsx'
-import './App.css';
-import Clima from './Components/Clima/Clima.jsx'
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import "./App.css";
 
-function App() {
-    
-    /* 
-    const date = new Date()
-    const hora = date.getHours()
-
-    let bg
-
-    if (hora <= 6 || hora >= 18) {
-        bg = bg0
-    } else {
-        bg = bg1
-    }
-
-    //data
-    var dia = custom_date()
-    var loc = "Manaus - AM"
-
-    //temperatura
-    var temp = Math.round(Math.random() * 18) + 15,
-        t_max = (temp + 3),
-        t_min = (temp - 2),
-        s_temp
-    if (temp > 25) {
-        s_temp = Math.floor(temp * 1.1)
-    } else {
-        s_temp = Math.floor(temp * 0.95)
-    }
-
-
-    //clima
-    var climas = ['Ensolarado', 'Parcialmente ensolarado', 'Nublado']
-    var clima
-    var c_url
-
-    climaf()
-
-    //setup clima
-    function climaf() {
-        if (temp < 22) {
-            clima = climas[2]
-            c_url = nublado
-
-        } else if (temp < 28) {
-            clima = climas[1]
-            c_url = parc_ensolarado
-
-        } else if (temp < 34) {
-            clima = climas[0]
-            c_url = ensolarado
-
-        }
-    }
-    //data atual
-    function custom_date() {
-        let today = new Date();
-        let custom_months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-        let custom_days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-        let date = custom_days[today.getDay()] + ", " + today.getDate() + " de " + custom_months[today.getMonth()];
-        return date;
-    }
- */
-    return (
-        <div id='corpo'>
-            <div id="page">
-                <Header />
-                
-                <Clima/>
-
-                <section id='body'>
-                    <div id='conteudo'>
-                        <div id='noticias'>
-                            <p id='noticiasTitulo'><b>Notícias</b></p>
-                            <Noticia
-                                imagemSrc={'https://tinyurl.com/3p2sa6at'}
-                                linkNoticia={'https://g1.globo.com/ms/mato-grosso-do-sul/noticia/2023/06/17/por-que-gado-morre-de-frio-entenda-os-motivos-para-hipotermia-dos-animais-no-pantanal.ghtml'}
-                                titulo={'Por que gado morre de frio? Os motivos para hipotermia dos animais no Pantanal'}
-                                descricao={'Mais de 1 mil cabeças de gado morreram de frio em Mato Grosso do Sul nos últimos dias. De acordo com especialistas, as mortes dos animais estão atreladas a uma severa hipotermia e o gado pantaneiro pode sofrer ainda mais com o frio.'}
-                                creditos={'g1.globo.com.br/'}
-                            />
-                            <Noticia
-                                imagemSrc={'https://tinyurl.com/2x3ht8sh'}
-                                linkNoticia={'https://www.greenpeace.org/brasil/imprensa/um-ano-das-chuvas-em-recife-tragedia-pode-voltar-a-ocorrer-se-nao-houver-acao-do-poder-publico/'}
-                                titulo={'Um ano das chuvas em Recife: tragédia pode voltar a ocorrer se não houver ação do poder público'}
-                                descricao={'Após um ano do maior desastre em Pernambuco, fortes chuvas deixam o Estado novamente em alerta. Greenpeace Brasil apoia a revisão do Plano Nacional de Adaptação para evitar novos desastres'}
-                                creditos={'greenpeace.org/'}
-                            />
-                            <Noticia
-                                imagemSrc={'https://tinyurl.com/ymdu6z8r'}
-                                linkNoticia={'https://www.greenpeace.org/brasil/imprensa/abril-registra-queda-de-67-9-no-desmatamento-na-amazonia-em-relacao-ao-mesmo-periodo-em-2022/'}
-                                titulo={'Abril registra queda de 67.9% no desmatamento na Amazônia, em relação ao mesmo período em 2022'}
-                                descricao={'Abril registra queda de 67.9% no desmatamento na Amazônia, em relação ao mesmo período em 2022.'}
-                                creditos={'greenpeace.org/'}
-                            />
-                        </div>
-                        <div id='sideContent'>
-                            <Previsao />
-                            <div id='atalhos'>
-                                <Atalho
-                                    titulo={'Dicas'}
-                                    descricao={'Veja aqui dicas de como cuidar do meio ambiente com pequenas ações no dia a dia'}
-                                    imagem={'https://cdn-icons-png.flaticon.com/512/361/361892.png'}
-                                    link={'/Teste'}
-                                />
-                                <Atalho
-                                    titulo={'Agenda'}
-                                    descricao={'Planeje seus eventos, atividade ou lembretes em um só lugar'}
-                                    imagem={'https://cdn-icons-png.flaticon.com/512/363/363485.png'}
-                                    link={''}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div >
-            <Footer />
-        </div>
-    )
-}
-
-export default App
+const App = () => {
+  return (
+    <div id="App">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Dicas from "./Components/Dicas/Dicas.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Teste from "./teste.jsx";
-
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App/>
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/dicas",
+        element: <Dicas />,
+      }
+    ]
   },
-  {
-    path: '/teste',
-    element: <Teste/>
-  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
