b9fcc0af89d2ff6d404dd49f60fc237f3c177b93
HernanJr0 <hernanjunior90@gmail.com>
2023-08-17 23:32:51 -0400

    material ui codado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const App = () => {
   return (
     <div id="App">
--- src/Components/Atalho/Atalho.jsx
+++ src/Components/Atalho/Atalho.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './Atalho.css'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Atalho.css";
+import { Button } from "@mui/material";
 
 class Atalho extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            titulo: this.props.titulo,
-            descricao: this.props.descricao,
-            imagem: this.props.imagem,
-            link: this.props.link
-        }
-    }
-    render() {
-        return (
-            <div id='Atalho' >
-
-
-                <div id='atalhoCont'>
-                    <h2 id='atalhoTitulo'>{this.state.titulo}</h2>
-                    <hr />
-                    <div id='atalhoDesc'>
-                        <div>{this.state.descricao}</div>
-                        <img src={this.state.imagem} id='atalhoImg' />
-                    </div>
-                    <Link id='atalhoLink' to={this.state.link}>Ver mais...</Link>
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      titulo: this.props.titulo,
+      descricao: this.props.descricao,
+      imagem: this.props.imagem,
+      link: this.props.link,
+    };
+  }
+  render() {
+    return (
+      <div id="Atalho">
+        <div id="atalhoCont">
+          <h2 id="atalhoTitulo">{this.state.titulo}</h2>
+          <hr />
+          <div id="atalhoDesc">
+            <div>{this.state.descricao}</div>
+            <img src={this.state.imagem} id="atalhoImg" />
+          </div>
+          <Link id="atalhoLink" to={this.state.link}>
+            <Button variant="text">Ver mais...</Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Atalho
+export default Atalho;
