969f8e1fcb123352ecfae2361164e1231e7f26ee
Hernan Toro Junior <113436068+HernanJr0@users.noreply.github.com>
2023-08-22 01:25:51 -0400

    search bar parcial

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,36 @@
-import './Header.css'
-import logo from '../../assets/ecoLogoWhite.svg'
-import search_black from '../../assets/search_black.svg'
+import { useState } from 'react'
+import "./Header.css";
+import logo from "../../assets/ecoLogoWhite.svg";
+import search_black from "../../assets/search_black.svg";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { IconButton, TextField } from '@mui/material';
+import Search from '@mui/icons-material/Search'
 
 function Header() {
+
     return (
         <nav>
-            <div id='topbar'>
-                <Link to='/' id='c_logo'>
-                    <img id='logo' src={logo} alt='logo' />
+            <div id="topbar">
+                <Link to="/" id="c_logo">
+                    <img id="logo" src={logo} alt="logo" />
                 </Link>
-                <div id='search'>
-                    <input id='in-search' type='text' placeholder='Buscar' />
-                    <button id='b-search'><img src={search_black} alt='search-icon' /></button>
-                </div>
+
+                <TextField
+                    id='search'
+                    label="Buscar"
+                    variant="outlined"
+                    size='small'
+                    InputProps={{
+                        endAdornment: (
+                            <IconButton edge="end">
+                                <Search />
+                            </IconButton>
+                        ),
+                    }}
+                />
             </div>
         </nav>
-    )
+    );
 }
-export default Header
+export default Header;
