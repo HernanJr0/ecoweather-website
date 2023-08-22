15559bd86b2525a875e3c1b5e418a770017cd0ad
HernanJr0 <hernanjunior90@gmail.com>
2023-08-21 20:46:53 -0400

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 
 import '@fontsource/roboto/300.css';
@@ -14,6 +15,7 @@ const App = () => {
       <Header />
       <Outlet />
       <Footer />
+      <Analytics/>
     </div>
   );
 };
