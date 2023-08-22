f71d7b9ea9a4592cf626262e2aac67cd4237dcc8
Hernan Toro Junior <113436068+HernanJr0@users.noreply.github.com>
2023-08-22 09:12:43 -0300

    Update Clima.jsx

--- src/Components/Clima/Clima.jsx
+++ src/Components/Clima/Clima.jsx
@@ -72,6 +72,7 @@ class Clima extends Component {
 
         if (new Date().getHours() >= 6 && new Date().getHours() <= 18) {
             document.querySelector('#bg').src = 'http://source.unsplash.com/960x540/?' + d.weather[0].description + this.state.loc;
+            console.log(this.state.loc)
         } else {
             document.querySelector('#bg').src = 'http://source.unsplash.com/960x540/?night-' + d.weather[0].description + this.state.loc;
         }
