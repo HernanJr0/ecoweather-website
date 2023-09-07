4a02595b88a0b0731a552f92cb3e1ad546aa6d10
wMzley <wesleyfelipe1904@gmail.com>
2023-09-07 00:48:35 -0400

    noticias dinamicas and some minor improviments

---
+++ src/Components/Noticia/GridNoticia.jsx
@@ -0,0 +1,28 @@
+import Noticia from '../../Components/Noticia/Noticia.jsx';
+import '../../Components/Noticia/GridNoticia.css'
+import { useState } from "react";
+
+function GridNoticia() {
+    const [items, setItems] = useState([])
+
+    window.onload = () => {
+        fetch("https://newsapi.org/v2/everything?q=meio-ambiente" + "&searchIn=title" + "&sortBy=relevancy" + "&pageSize=10" + "&language=pt" + "&apiKey=cd3417de6c7b4ae38a0cff0124696c5d")
+            .then((resp) => { return resp.json() })
+            .then((data) => {
+                setItems(data.articles)
+                console.log(data)
+            })
+    };
+
+    return (
+        <div>
+            <p id='noticiasTitulo'><b>Notícias</b></p>
+            
+            {items.map((item, i) => (
+                <Noticia key={i} item={item} />
+            ))}
+        </div>
+    )
+}
+
+export default GridNoticia
