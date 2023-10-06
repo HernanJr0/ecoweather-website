1a68e7dea4d8397e48a399e2e0dc1c7fa2fef9c3
wMzley <wesleyfelipe1904@gmail.com>
2023-10-05 21:13:46 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@
 // import { Analytics } from "@vercel/analytics/react";
 
 import "./App.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 // import { createTheme, ThemeProvider } from "@mui/material";
 import { AuthGoogleProvider } from "./contexts/authGoogle";
